0b837406d9d20f84a60181e16dc0f3fb584e18c3
Erkam USLU <ercome1994gmail.com>
2022-04-21 10:42:08 +0300

    erkam2commit

--- src/package01/C02.java
+++ src/package01/C02.java
@@ -3,5 +3,6 @@ package package01;
 public class C02 {
     public static void main(String[] args) {
         System.out.println("ERKAM 2. CLASS");
+        System.out.println("ne yazsam bilemedim...");
     }
 }
