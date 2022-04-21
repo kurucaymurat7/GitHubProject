7e5c84e928a3713569c232ceb182ff38e6c7f8e1
Erkam USLU <ercome1994gmail.com>
2022-04-21 10:26:08 +0300

    erkam

--- src/package01/C01.java
+++ src/package01/C01.java
@@ -3,5 +3,7 @@ package package01;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("ERKAM'DAN SELAMLAR");
     }
+
 }
--- src/package01/C02.java
+++ src/package01/C02.java
@@ -1,4 +1,7 @@
 package package01;
 
 public class C02 {
+    public static void main(String[] args) {
+        System.out.println("ERKAM 2. CLASS");
+    }
 }
